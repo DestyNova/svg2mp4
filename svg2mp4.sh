0443015580378a80aa42365f23bb2d314f220cba
Oisín Mac Fhearaí <denpashogai@gmail.com>
2018-08-05 03:33:35 +0100

    * Rework command-line option parsing * Pass some important arguments to FFmpeg that were completely ignored before * Add usage message

--- svg2mp4.sh
+++ svg2mp4.sh
@@ -2,17 +2,63 @@
 
 set -euo pipefail
 
+usage() {
+  echo "Usage: $0 [opts] <filename>
+  Options:
+    -w <WIDTH>: Horizontal resolution
+    -h <HEIGHT>: Vertical resolution
+    -f <FPS>: Output FPS
+    -t <TIME>: Maximum file duration in seconds" 1>&2
+  exit 1
+}
+
+# defaults
+FPS=30
+TIME=2
+RES_X=1280
+RES_Y=720
+JS_PATH=`dirname $0`/svg-to-mp4.js
+
+while getopts ":w:h:f:t:" opt; do
+  case $opt in
+    w)
+      RES_X=$OPTARG
+      ;;
+    h)
+      RES_Y=$OPTARG
+      ;;
+    f)
+      FPS=$OPTARG
+      ;;
+    t)
+      TIME=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+if [ $# -ne 1 ]; then
+  usage
+fi
+
 IN_FILE=$1
 IN_PATH="file://`pwd`/$IN_FILE"
-FPS=${2:-30}
-TIME=${3:-5}
-OUT_FILE=${4:-"out.mp4"}
-RES_X=${5:-1280}
-RES_Y=${6:-720}
+OUT_FILE=${2:-"out.mp4"}
 
-JS_PATH=`dirname $0`/svg-to-mp4.js
+if [ ! -e $IN_FILE ]; then
+  echo "ERROR: Can't find input file \"$IN_FILE\"" >&2
+  exit 1
+fi
 
 echo "Converting up to $TIME seconds of $IN_PATH at $FPS fps. Output file: $OUT_FILE"
 
 # Note: swapping stdout / stderr to work around ne'er-to-be-fixed PhantomJS bug
-(phantomjs $JS_PATH $IN_PATH $TIME $FPS $RES_X $RES_Y 3>&2 2>&1 1>&3-) | ffmpeg -y -c:v png -f image2pipe -r 30 -t 4 -i - -c:v libx264 -pix_fmt yuv420p -vf scale=$RES_X:$RES_Y $OUT_FILE
+(phantomjs $JS_PATH $IN_PATH $TIME $FPS $RES_X $RES_Y 3>&2 2>&1 1>&3-) | ffmpeg -y -c:v png -f image2pipe -r $FPS -t $TIME -i - -c:v libx264 -pix_fmt yuv420p -vf scale=$RES_X:$RES_Y $OUT_FILE
